3f7521f3cffb51a31e95ff0a23c3e1b12f80e8cf
Martin Kreuz <martin@kreuz.me>
2023-06-27 18:48:50 +0200

    added debug message

--- mandarintrainer/main.py
+++ mandarintrainer/main.py
@@ -4,8 +4,16 @@ import classes.datachooser as dc
 def main(args):
     
     dc.datachooser(args[1])
-    
-#if name 
+
+#####################   
+#   !! DEBUG !!     #
+#####################
+#                   #
+#  DELETE BEFORE    #
+#   MERGING TO      #
+#     MAIN!         #
+#                   #
+#####################
 
 if __name__ == "__main__":
     main(sys.argv)
