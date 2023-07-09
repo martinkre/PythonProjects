360b6468782cf764684226b97814268f1156aab3
Martin Kreuz <martin@kreuz.me>
2023-07-09 22:17:51 +0200

    added content switcher and edit mode to add new

--- mandarintrainer/classes/appWindow.py
+++ mandarintrainer/classes/appWindow.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QAction, QTableView
+from PyQt5.QtWidgets import QApplication, QMainWindow, QAction, QPushButton
 from PyQt5.QtGui import QStandardItemModel, QStandardItem
 from ui.MainWindow2 import Ui_MainWindow
 
@@ -13,18 +13,31 @@ class MyApp(QMainWindow):
         self.ui.setupUi(self)
         self.setWindowTitle("Mandarin Trainer App")
         
+        
 
         exit_action = QAction('Exit', self)
         exit_action.setShortcut('Ctrl+Q')
         exit_action.triggered.connect(self.exit_application)
         
         self.ui.actionExit.triggered.connect(self.exit_application)
-        self.ui.pushButton_4.setText("Activated")
-        self.ui.pushButton_4.pressed.connect(self.checker_button)
+        
 
-        self.load_data()
+        self.ui.actionSide_Menu.triggered.connect(self.toggle_side_menu)
+        
+        
+
+        #self.load_data()
         self.build_text()
     
+    def toggle_side_menu(self):
+        sm = self.ui.SideMenu
+        button = self.ui.actionSide_Menu
+        if button.isChecked():
+            sm.show()
+        else:
+            sm.hide()
+        
+
     def call_loader():
         app = QApplication(sys.argv)
         window = MyApp()
@@ -36,26 +49,49 @@ class MyApp(QMainWindow):
         
         if self.ui.pushButton_4.isChecked():
             #self.ui.pushButton_4.setChecked = 0
-            self.ui.pushButton_4.setText("Deactivated")
+            self.ui.pushButton_4.setText("Edit deactivated")
+            self.button_add.hide()
         else:
             #self.ui.pushButton_4.setChecked = 1
-            self.ui.pushButton_4.setText("Activated")
+            self.ui.pushButton_4.setText("Edit activated")
+            self.button_add.show()
+            
+                    
+            
 
     def build_text(self):
         self.ui.pushButton.setText("Exit")
         self.ui.pushButton_2.setText("Top 100")
         self.ui.pushButton_3.setText("Numbers")
         self.ui.pushButton_5.setText("Weekdays")
+        self.ui.pushButton_4.setText("Edit activated")
+
+        self.button_add = QPushButton(self.ui.SideMenu)
+        self.ui.verticalLayout.insertWidget(3, self.button_add)
+        self.button_add.setText("+")
+
+        fp_top100 = "./data/mandb.csv"
+        fp_numbers = "./data/numdb.csv"
+        fp_weekdays = "./data/weekdays.csv"
         
         self.ui.pushButton.pressed.connect(self.exit_application)
-        #self.ui.pushButton_2.pressed.connect()
-        #self.ui.pushButton_3.pressed.connect()
-        #self.ui.pushButton_5.pressed.connect()
+        self.ui.pushButton_2.pressed.connect(lambda: self.load_data(fp_top100))
+        self.ui.pushButton_3.pressed.connect(lambda: self.load_data(fp_numbers))
+        self.ui.pushButton_5.pressed.connect(lambda: self.load_data(fp_weekdays))
+        self.button_add.pressed.connect(lambda: self.add_items("test"))
+        
+        self.ui.pushButton_4.pressed.connect(self.checker_button)
+
+
+    def add_items(self, name):
+        new_button = QPushButton(self.ui.SideMenu)
+        new_button.setText(name)
+        self.ui.verticalLayout.insertWidget(3, new_button)
         
 
-    def load_data(self):
+    def load_data(self, filepath):
         # Path to your CSV file
-        csv_file = './data/numdb.csv'
+        csv_file = filepath #'./data/mandb.csv'
 
         # Read CSV data
         with codecs.open(csv_file, "r", encoding="utf-8-sig") as file:
